70f100ce4d08f763f001dd38bef1af14f4ccfaec
jadilovic <adilovic79@yahoo.com>
2019-10-26 21:54:56 +0200

    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.app.flight_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
